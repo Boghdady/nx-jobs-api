546db836f019a5b171fb20f5a272c1060b861592
boghdady <boghdady107@gmail.com>
2025-02-15 15:27:47 +0200

    generate access token

--- apps/jobs-auth/src/app/auth/auth.module.ts
+++ apps/jobs-auth/src/app/auth/auth.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     ConfigModule,
     JwtModule.registerAsync({
+      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         secret: configService.getOrThrow<string>('JWT_SECRET'),
         signOptions: {
@@ -17,6 +20,6 @@ import { UsersModule } from '../users/users.module';
     }),
     UsersModule,
   ],
-  providers: [],
+  providers: [AuthService, AuthResolver],
 })
 export class AuthModule {}
--- apps/jobs-auth/src/app/auth/auth.service.ts
+++ apps/jobs-auth/src/app/auth/auth.service.ts
@@ -1,8 +1,11 @@
+import { Response } from 'express';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcryptjs';
+import { LoginInput } from './dto/login.input';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +14,36 @@ export class AuthService {
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService
   ) {}
+
+  async login({ email, password }: LoginInput, response: Response) {
+    // 1) Verify user and password
+    const user = await this.verifyUser(email, password);
+
+    // 2) Generate expire time for cookie that will be the same as the generated jwt
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getTime() +
+        parseInt(this.configService.getOrThrow('JWT_EXPIRATION_MS'), 10)
+    );
+
+    // 3) Generate the jwt payload
+    const TokenPayload: TokenPayload = {
+      id: user.id,
+    };
+
+    // 4) Generate the access token
+    const accessToken = this.jwtService.sign(TokenPayload);
+
+    // 5) Set the access token in the cookie
+    response.cookie('Authentication', accessToken, {
+      httpOnly: true,
+      expires,
+      secure: this.configService.getOrThrow('NODE_ENV') === 'production',
+    });
+
+    // 6) Return the user
+    return user;
+  }
 
   private async verifyUser(email: string, password: string) {
     try {
