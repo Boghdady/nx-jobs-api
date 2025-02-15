431bec04cdb19fe3cc03e4de8ae4d24f397e1f3f
boghdady <boghdady107@gmail.com>
2025-02-15 14:03:52 +0200

    Register JWT Module

---
+++ apps/jobs-auth/src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>('JWT_EXPIRATION_MS'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  providers: [],
+})
+export class AuthModule {}
--- apps/jobs-auth/src/main.ts
+++ apps/jobs-auth/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +18,7 @@ async function bootstrap() {
       whitelist: true,
     })
   );
-  const port = process.env.PORT || 3000;
+  const port = app.get(ConfigService).get('AUTH_PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
